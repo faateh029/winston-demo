c4e29306eac8b44cfb4ed0030878f56412666969
faateh029 <buttfatih691@gmail.com>
2025-08-08 16:41:26 +0500

    Logging erros with winston

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import winston from 'winston';
 
 const {format} = winston;
-const {combine , timestamp , json , prettyPrint} = format;
+const {combine , timestamp , json , prettyPrint , errors} = format;
 
 const logger = winston.createLogger({
     level:'info', //global minimum level for all transports , acts as a master filter for all logs . this means only levels of the default and above will be logged . no logs with level bellow the default level will be logged
     format:combine(
+        errors({stack:true}), //stack shows the complete execution path towards the error
         timestamp(),
         json(),
         prettyPrint()
@@ -18,5 +19,5 @@ const logger = winston.createLogger({
 })
 const requestLog = {method:"GET",isAuthenticated:"Yes"}; //adding custom loggin in the log messages
 const childLogger = logger.child(requestLog);
-childLogger.info("An info level log");
-childLogger.error("errors are logged here");
+childLogger.info("An info level log" , new Error);
+childLogger.error("errors are logged here" , new Error);
