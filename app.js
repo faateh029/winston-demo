30d1a285d296c39273d9af6318c4d55c1da9d866
faateh029 <buttfatih691@gmail.com>
2025-08-08 15:55:47 +0500

    adding custom logging

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import winston from 'winston';
 
 const {format} = winston;
-const {combine , timestamp,printf} = format;
+const {combine , timestamp , json} = format;
 
 const logger = winston.createLogger({
     level:'info', //global minimum level for all transports , acts as a master filter for all logs . this means only levels of the default and above will be logged . no logs with level bellow the default level will be logged
     format:combine(
         timestamp(),
-            printf((info)=> `${info.timestamp} , ${info.message}`) 
+        json()
+           // printf((info)=> `${info.timestamp}  ${info.level} : ${info.message}`) 
     ),
     transports:[
         new winston.transports.Console(),
         //new winston.transports.File({filename:'app.log' , level:'error'})
+        
     ]
 })
-logger.info("An info level log");
-logger.error("errors are logged here");
+const requestLog = {method:"GET",isAuthenticated:"Yes"}; //adding custom loggin in the log messages
+logger.info("An info level log" , requestLog);
+logger.error("errors are logged here",requestLog);
